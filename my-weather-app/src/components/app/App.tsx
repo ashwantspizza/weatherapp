12ae589b02d7f0367aaad711395ad3cf5717d245
ashbsmith <aislingbrogansmith@gmail.com>
2023-09-04 17:23:50 +0100

    serachbar component and favicon.ico added in public

--- my-weather-app/src/components/app/App.tsx
+++ my-weather-app/src/components/app/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 // import './App.css';
 import WeatherData from '../../components/weatherdata/WeatherData';
+import SearchBar from '../../components/searchbar/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <WeatherData />
+      <SearchBar />
     </div>
   );
 }
--- my-weather-app/src/components/searchbar/SearchBar.tsx
+++ my-weather-app/src/components/searchbar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useState} from 'react';
 
 interface SearchProps {
     buttonOnClick: (city: string) => void;
