db147113c641155e817c37bc227eb3636c956dac
ashbsmith <aislingbrogansmith@gmail.com>
2023-09-06 19:24:58 +0100

    getting user input to come up on screen

--- my-weather-app/src/components/weatherdata/WeatherData.tsx
+++ my-weather-app/src/components/weatherdata/WeatherData.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const apiKey = 'eb58f7958543d47270277234019d256e';
 
 export async function getWeatherData(cityChoice: string) {
-  const res = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}');
+  const res = await fetch(`$https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`);
   const data = await res.json();
   console.log(res);
 
