1c901cfe0461bcd72a1906e64a2dacd765aedf45
ashbsmith <aislingbrogansmith@gmail.com>
2023-10-07 10:44:26 +0100

    icon from api added woooo

--- my-weather-app/src/components/weatherdata/WeatherData.tsx
+++ my-weather-app/src/components/weatherdata/WeatherData.tsx
@@ -1,4 +1,5 @@
 const apiKey = 'eb58f7958543d47270277234019d256e';
+const makeIconUrl = (iconId: number) => `https://openweathermap.org/img/wn/${iconId}@2x.png`
 
 export async function getWeatherData(cityChoice: string) {
   const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityChoice}&appid=${apiKey}`);
@@ -7,10 +8,11 @@ export async function getWeatherData(cityChoice: string) {
   console.log(data);
 
   const { weather, main: { temp, feels_like, temp_min, temp_max, pressure, humidity }, wind: { speed }, sys: { country }, name } = data;
-  const { description } = weather[0];
+  const { description, icon } = weather[0];
 
   return {
     description,
+    iconURL: makeIconUrl(icon),
     temp,
     feels_like,
     temp_min,
